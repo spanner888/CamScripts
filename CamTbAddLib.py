cfd381e07facfec5811004fcd65374cc3c38d6f7
spanner888 <spanner888@usabledevices.com>
2024-08-14 19:15:54 +1000

    removed bulk (all?) recent sidetrack code on globals

--- CamTbAddLib.py
+++ CamTbAddLib.py
@@ -125,10 +125,6 @@ def toolBitNew(library, filename, shape_name, shape_full_path_fname, attrs):
 
 
 def addToolToCurrentLibrary(library, shape_name, tool_props, tb_nr, tb_name_rules):
-
-    # FIXME review if global requirefd..think should be
-    # global all_shape_attrs
-
     # USE THE NEW TB_NAME_TMPLATE TO CHANGE endmill_tool_props["name"]..using TB values
     tb_name = create_tb_name(tb_name_rules, tb_nr, tool_props)
     tool_props["name"] = tb_name
@@ -141,42 +137,9 @@ def addToolToCurrentLibrary(library, shape_name, tool_props, tb_nr, tb_name_rule
         shape_full_path_fname = workingdir + "/Shape/" + shape_name + ".fcstd"
         shape_full_path_fname_as_path = osPath(shape_full_path_fname)
 
-        #  TODO : get shapes ...then again????
-        # for FC code...need use real/existing shape OK
-        # is here supposed to merge users shape props with tempale/existing shape??
-        # shoulD be ABLE TO MOVE THIS TO top THIS MODULES - NO NEED REREAD EVERY TIME
         if shape_full_path_fname_as_path.is_file():
-            # FIXME: DELETE NEXT, THEN USE (renamed) shape_full_path_fname_attrs_NEW
-            # shape_name, shape_full_path_fname_attrs = getToolShapeProps(workingdir + "/Shape/", shape_name)
-            # shape_full_path_fname_attrs_NEW = all_shape_attrs[shape_name]
             shape_full_path_fname_attrs = all_shape_attrs[shape_name]
 
-            #print()
-            #print(shape_full_path_fname_attrs_NEW, "addToolToCurrentLibrary: shape_full_path_fname_attrs_NEW")
-            #print_Tb(all_shape_attrs, msg="show imp again:")
-            #print()
-
-            # print(shape_full_path_fname_attrs)
-            # print()
-            # print(shape_full_path_fname_attrs_NEW)
-            #print()
-            #if shape_full_path_fname_attrs == shape_full_path_fname_attrs_NEW:
-                #print("yea the SAME")
-            #else:
-                #print("o oh!!!!!")
-            #print()
-            #
-            # for k, v in shape_full_path_fname_attrs:
-            #     if k in shape_full_path_fname_attrs_NEW:
-            #         if v == shape_full_path_fname_attrs_NEW[k]:
-            #             print(k, "BOTH values = ", v)
-            #         else:
-            #             print(k, "values DIFFER ", v, shape_full_path_fname_attrs_NEW[k])
-            #
-            #     else:
-            #         print("Key {} not present in new attrs")
-
-            # "TODO swap order of vars ABOVE to check presence in New  & Not in old"
             params = shape_full_path_fname_attrs["parameter"]
 
             new_tool_params = tool_props["parameter"]
@@ -249,29 +212,11 @@ def processUserToolInput(tb_name_rules,
     # need any document open, no changes are made.
     if FreeCAD.ActiveDocument == None:
         doc = FreeCAD.newDocument()
-   
-    # global all_shape_attrs
 
-    # update tool_props with dia, tb_base_name
-    # tool_props = all_shape_attrs[shape_name]deepcopy()
-    # tool_props_orig = dict()
-    # tool_props = dict()
-    # tool_props_orig = all_shape_attrs[shape_name]
-
-    #     ok - fails as can't deep copt FC:Quantity
-    #     tool_props = copy.deepcopy(tool_props_orig)
-    #     so to see if issue here or afterwards somewhere
-    #
-    #     just create a HARDCODED tool_props and see what happens!!!
-    # or create class-var for all_shape_attrs...
-    # still wondering if somewhere pass WRONG var & accidently change global var
-    # BUT can see change immediately herE so ?????
-    # tool_props = {'shape': 'endmill.fcstd', 'name': 'default_em', 'parameter': {'CuttingEdgeHeight': 30.0 mm, 'Diameter': 8.12, 'Length': 50.0 mm, 'ShankDiameter': 3.0 mm}, 'attribute': {'Chipload': 0.0 mm, 'Flutes': 0, 'Material': 'HSS', 'SpindleDirection': 'Forward'}}
-
-    # tool_props_str = str(all_shape_attrs[shape_name])
+    # Cannot copy/deepcopy all_shape_attrs[] due to the FC Quantities
+    # Not removing Quantities as really want those to help validate
+    # lots Speed&Feeds calc with diff units & vary dif data sources...
     tool_props_str = "{"
-    #q = FreeCAD.Units.Quantity
-
     for k, v in all_shape_attrs[shape_name].items():
         if k == "parameter":
             tool_props_str += "'parameter': {"
@@ -282,42 +227,13 @@ def processUserToolInput(tb_name_rules,
         elif k == "attribute":
             tool_props_str += "'attribute' : {"
             for k1, v1 in v.items():
-                #print(k1, v1 )
-                
-                #fails on material = STR already
-                #so do convert in method that checks if quantity or str...
-                #print(type(v1))
                 tool_props_str += "'" + k1 + "': '" + str(v1) + "', "
         else:
             tool_props_str += "'" + k + "': '" + v + "', "
-            # tool_props_str += "{'" + k + "': '" + v['shape'] + "','"
-            # tool_props_str += "{'" + k + "': '" + v['name']  + "','"
     tool_props_str += "}}"
 
-    #print(tool_props_str)
     import ast
-    # Fails as Values+ Units are NOT quoted in the text!!
     tool_props = ast.literal_eval(tool_props_str)
-
-    # tool_props = dict(tool_props_orig)
-    # so json or print each element to str & make sure quoted, then literal_eval?
-
-    # import json
-    # ALSO fails due to missing quotes: Expecting property name enclosed in double quotes: line 1 column 2 (char 1)
-    # tool_props = json.loads(tool_props_str)
-    # print(tool_props['meta']['total_count'])
-
-    #print(tool_props, "processUserToolInput: tool_props  ast.literal_eval, before update")
-    #tool_props["name"] = tb_base_name
-    #tool_props["parameter"]["Diameter"] = dia
-    #print()
-    #print(tool_props)
-
-    #print()
-    #print(tool_props, "processUserToolInput: tool_props copy, AFTER update")
-    #print()
-    #print(all_shape_attrs[shape_name], "processUserToolInput: all_shape_attrs[shape_name]")
-    #print()
 
     library = PathToolBitLibraryGui.ToolBitLibrary()
     workingdir = None
